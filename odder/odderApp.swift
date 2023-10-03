a4c3b82024952ab348530d681a0bc89ae6028f9c
Vauzher <vawjer.dusky@gmail.com>
2023-10-03 15:07:32 +0200

    Initial Commit

---
+++ odder/odderApp.swift
@@ -0,0 +1,17 @@
+//
+//  odderApp.swift
+//  odder
+//
+//  Created by Vauzher on 03.10.23.
+//
+
+import SwiftUI
+
+@main
+struct odderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
